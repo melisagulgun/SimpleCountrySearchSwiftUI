795f876ce6dd8b259662f27bcf5fc73d5da345f4
Melisa Gülgün Dombaycı <melissagulgun@gmail.com>
2024-08-31 13:11:11 +0300

    Initial Commit

---
+++ SimpleCitySearchSwiftUI/SimpleCitySearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCitySearchSwiftUIApp.swift
+//  SimpleCitySearchSwiftUI
+//
+//  Created by Melisa Gülgün on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCitySearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleCitySearchSwiftUITests/SimpleCitySearchSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCitySearchSwiftUITests.swift
+//  SimpleCitySearchSwiftUITests
+//
+//  Created by Melisa Gülgün on 31.08.2024.
+//
+
+import Testing
+@testable import SimpleCitySearchSwiftUI
+
+struct SimpleCitySearchSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
